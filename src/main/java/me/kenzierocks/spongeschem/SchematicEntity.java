72e08c1d2edc05c70f43015d6e1bb02b74a4145e
Kenzie Togami <ket1999@gmail.com>
2016-03-16 20:06:22 -0700

    Add a little stuff to kick off PR

--- src/main/java/me/kenzierocks/spongeschem/SchematicEntity.java
+++ src/main/java/me/kenzierocks/spongeschem/SchematicEntity.java
@@ -5,6 +5,16 @@ import com.google.auto.value.AutoValue;
 @AutoValue
 public abstract class SchematicEntity extends VersionedContentHolder {
 
+    public static final int CURRENT_CONTENT_VERSION = 1;
+
+    public static final SchematicEntity create() {
+        return create(CURRENT_CONTENT_VERSION);
+    }
+
+    public static final SchematicEntity create(int contentVersion) {
+        return new AutoValue_SchematicEntity(contentVersion);
+    }
+
     SchematicEntity() {
     }
 

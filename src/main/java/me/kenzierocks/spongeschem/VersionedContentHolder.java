507c64d49c133ba556845fe90ff99988d16f4ef3
Kenzie Togami <ket1999@gmail.com>
2016-03-16 20:13:06 -0700

    Kill off terrible default implementations

--- src/main/java/me/kenzierocks/spongeschem/VersionedContentHolder.java
+++ src/main/java/me/kenzierocks/spongeschem/VersionedContentHolder.java
@@ -2,40 +2,6 @@ package me.kenzierocks.spongeschem;
 
 public abstract class VersionedContentHolder {
 
-    public static abstract class Immutable extends VersionedContentHolder {
-
-        protected final int contentVersion;
-
-        protected Immutable(int contentVersion) {
-            this.contentVersion = contentVersion;
-        }
-
-        @Override
-        public int getContentVersion() {
-            return this.contentVersion;
-        }
-
-    }
-
-    public static abstract class Mutable extends VersionedContentHolder {
-
-        protected int contentVersion;
-
-        protected Mutable(int contentVersion) {
-            this.contentVersion = contentVersion;
-        }
-
-        @Override
-        public int getContentVersion() {
-            return this.contentVersion;
-        }
-
-        public void setContentVersion(int contentVersion) {
-            this.contentVersion = contentVersion;
-        }
-
-    }
-
     public abstract int getContentVersion();
 
 }
